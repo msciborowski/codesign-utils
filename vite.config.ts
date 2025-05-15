3584a6d0d6b2ac442a829a2ac927d22fdd73aaf1
Marcin Ściborowski <9884143+msciborowski@users.noreply.github.com>
2025-05-15 15:32:56 +0200

    esmodules only

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,11 @@ export default defineConfig({
     lib: {
       entry: './src/index.ts', // Specifies the entry point for building the library.
       name: 'vite-react-ts-button', // Sets the name of the generated library.
-      fileName: format => `index.${format}`, // Generates the output file name based on the format.
-      formats: ['cjs', 'es'], // Specifies the output formats (CommonJS and ES modules).
+      fileName: format => `index.${format}.js`, // Generates the output file name based on the format.
+      formats: [
+        // 'cjs',
+        'es',
+      ], // Specifies the output formats (CommonJS and ES modules).
     },
     // rollupOptions: {
     //   external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
